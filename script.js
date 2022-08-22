2935bd77d20af351dd547ad8726da4104f18a5e6
Jonathan Hawes <jhawes580@gmail.com>
2022-08-21 23:36:16 -0700

    Allow adding books with full properties

--- script.js
+++ script.js
@@ -3,24 +3,55 @@ const closeButton = document.querySelector('fieldset > img');
 const newBookButton = document.querySelector('.addbook');
 const overlay = document.querySelector('.overlay');
 const form = document.getElementById('addbook');
+const options = document.querySelector('.options');
+const bookList = document.querySelector('.books-list');
+const bookCard = document.querySelector('.book-card');
 //////////////////////////////
 
 let myLibrary = [];
 
-function Book(name, author, pages, read) {
-    this.name = name;
+function Book(title, author, pages, read) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
+function addBooktoLibrary(event) {
+    // Stops page from refreshing
+    event.preventDefault();
 
-form.querySelector('button').addEventListener('click', addBooktoLibrary);
+    /* Create a new Book object with the submitted values,
+    and add it to the library */
+    let temp = new Book(form.elements['title'].value, form.elements['author'].value, form.elements['pages'].value, form.elements['read'].value)
+    myLibrary.push(temp);
 
-function addBooktoLibrary(e) {
-    console.log(e);
-    console.log(form.elements['pages'].value);
+
+    /* Setup a new card to add to the bookList grid with a clone
+    of the  default bookCard */
+    let tempCard = bookCard.cloneNode(true);
+
+    // Remove the class that sets display: none to the card
+    tempCard.classList.remove('delete');
+
+    /* Set the card values to the values of the book we just added
+    to the myLibrary Array */
+    tempCard.querySelector('.title').textContent = myLibrary[myLibrary.length-1].title;
+    tempCard.querySelector('.author').textContent = 'by ' + myLibrary[myLibrary.length-1].author;
+    tempCard.querySelector('.pages').textContent = myLibrary[myLibrary.length-1].pages + ' pages';
+
+    /* If the user hasn't read the book, add the unread CSS class */
+    let hasRead = myLibrary[myLibrary.length-1].read;
+    if (hasRead == 'false') {
+        tempCard.querySelector('.read').classList.add('unread');
+    }
+    bookList.appendChild(tempCard);
+
+    console.log(myLibrary[0].title);
 }
+
+form.addEventListener('submit', addBooktoLibrary);
+
 
 newBookButton.addEventListener('click', insertOverlay)
 closeButton.addEventListener('click', removeOverlay);
