4b0d7d245f1e81a3c40d851c130bc33a52db17ee
Jonathan Hawes <jhawes580@gmail.com>
2022-08-24 13:22:38 -0700

    Alter program from a normal constructor to class Functionality remains the same.

--- script.js
+++ script.js
@@ -15,12 +15,14 @@ closeButton.addEventListener('click', removeOverlay);
 let myLibrary = [];
 let uniqueID = 0;
 
-function Book(title, author, pages, read, id) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = id;
+class Book {
+    constructor(title, author, pages, read, id) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = id;
+    }
 }
 
 function addBooktoLibrary(event) {
